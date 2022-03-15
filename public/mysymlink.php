4d2ca328022b3401aee14d75f18626cac717fdc3
nhomkinhdadat <binhtrongcdt1@gmail.com>
2022-03-16 00:29:56 +0700

    fix

--- public/mysymlink.php
+++ public/mysymlink.php
@@ -1,6 +1,1 @@
-<?php
-$targetFolder = $_SERVER['DOCUMENT_ROOT'].'/storage/app/public';
-$linkFolder = $_SERVER['DOCUMENT_ROOT'].'/public/storage';
-symlink($targetFolder,$linkFolder);
-echo 'Symlink completed';
-?>
+<?php$targetFolder = $_SERVER['DOCUMENT_ROOT'].'/storage/app/public/public';$linkFolder = $_SERVER['DOCUMENT_ROOT'].'/public/storage';symlink($targetFolder,$linkFolder);echo 'Symlink completed';?>
