fa3c44cf6ee4cf59573b9c95214a3c986f691202
Binh Trong <binhtrongcdt1@gmail.com>
2022-03-23 11:01:33 +0700

    add adver

--- resources/views/inc/intercooperation.blade.php
+++ resources/views/inc/intercooperation.blade.php
@@ -16,9 +16,34 @@
       <div class="item"><a href="#"><img class="lazy"
       data-original="{{ $row->img_path }}?w=170&amp;amp;h=130&amp;amp;" /></a></div>
       @endforeach
-
+            <div class="item adverment">
+            <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-format="fluid"
+            data-ad-layout-key="-4w+dg-39-4q+x2"
+            data-ad-client="ca-pub-6820106529386853"
+            data-ad-slot="4610757293"></ins>
+            </div>
+            <div class="item adverment">
+                <ins class="adsbygoogle"
+                style="display:block"
+                data-ad-format="fluid"
+                data-ad-layout-key="-4w+dg-39-4q+x2"
+                data-ad-client="ca-pub-6820106529386853"
+                data-ad-slot="4610757293"></ins>
+                </div>
+            <div class="item adverment">
+                    <ins class="adsbygoogle"
+                    style="display:block"
+                    data-ad-format="fluid"
+                    data-ad-layout-key="-4w+dg-39-4q+x2"
+                    data-ad-client="ca-pub-6820106529386853"
+                    data-ad-slot="4610757293"></ins>
+            </div>
       </div>
-
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
       <div class="clr"></div>
 
 
